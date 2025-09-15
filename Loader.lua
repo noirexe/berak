9133d37abb711431f23ab01a144964962178d581
fumi_ <shimiizumori@gmail.com>
2025-09-16 00:59:44 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-local url = "https://raw.githubusercontent.com/WataXScript/WataXMountAtin/main/Loader/main.lua"
+local url = "https://raw.githubusercontent.com/noirexe/berak/refs/heads/main/jembut.lua"
 local s, e = pcall(function()
     return loadstring(game:HttpGet(url))()
 end)
